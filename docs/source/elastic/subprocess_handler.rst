22df897e43980ac1f5fd8dfc083abf0714b04d02
Cheng Ni <chengn@meta.com>
2024-03-03 19:01:01 -0800

    [Torch Elastic] Refactor SubprocessHandler to separate module for easier subclass (#120373)
    Summary:
    
    ## No Functional Change
    - Refactor Subprocess Handler into a separate folder for easier subclassing
    - SubprocessHandler
        - added `local_rank_id` in `SubprocessHandler` to make it available as a field in the class
        - pass in `local_rank_id` from subprocess start
    
    Test Plan: No functional changes.
    
    Reviewed By: kurman
    
    Differential Revision: D54038627

---
+++ docs/source/elastic/subprocess_handler.rst
@@ -0,0 +1,16 @@
+Subprocess Handling
+==================
+
+.. automodule:: torch.distributed.elastic.multiprocessing.subprocess_handler
+
+Retrieve SubprocessHandler
+---------------------------
+
+.. autofunction:: torch.distributed.elastic.multiprocessing.subprocess_handler.get_subprocess_handler
+
+SubprocessHandler
+---------------------
+
+.. currentmodule:: torch.distributed.elastic.multiprocessing.subprocess_handler.subprocess_handler
+
+.. autoclass:: SubprocessHandler
---
+++ torch/distributed/elastic/multiprocessing/subprocess_handler/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+from torch.distributed.elastic.multiprocessing.subprocess_handler.subprocess_handler import SubprocessHandler
+from torch.distributed.elastic.multiprocessing.subprocess_handler.handlers import get_subprocess_handler
+
+__all__ = ["SubprocessHandler", "get_subprocess_handler"]
